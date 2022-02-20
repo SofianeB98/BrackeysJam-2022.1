8cc5093b0e81a2c86f4d889a0a2d6c8bfdc9164d
Dreaa <alexandre@a-lucas.fr>
2022-02-20 17:11:00 +0100

    Menu buttons + play & quit functionnality

---
+++ Assets/Scripts/UiButton.cs
@@ -0,0 +1,24 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class UiButton : MonoBehaviour
+{
+    public void Quit()
+    {
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #else
+                Application.Quit();
+        #endif
+    }
+
+    public void Options()
+    {
+
+    }
+
+    public void Play(int s)
+    {
+        SceneManager.LoadScene(s);
+    }
+}
