50895075ace398f0c9c0f41432917859c58ccec5
Sofiane <sofiane.benembareck@hotmail.com>
2022-02-21 23:01:48 +0100

    Modify header

--- Assets/Scripts/Character/CharacterData/CharacterDashData.cs
+++ Assets/Scripts/Character/CharacterData/CharacterDashData.cs
@@ -7,10 +7,12 @@ public class CharacterDashData : ScriptableObject
     [Header("Dash")]
     [Tooltip("in Unity Unit/s")] public float DashSpeed = 5.0f;
     [Tooltip("in Unity Unit")] public float DashDistance = 1.0f;
-    [Tooltip("in seconds")] public float DashDuration = 1.0f;
     [Tooltip("in seconds")] public float DashLoadDuration = 1.0f;
     [Tooltip("in seconds")] public float DashRecoveryDuration = 1.0f;
     [Tooltip("in seconds")] public float InvincibleDuration = 1.0f;
+    
+    [Header("DONT TOUCH THESE FIELDS !")]
+    [Tooltip("in seconds")] public float DashDuration = 1.0f;
     
     private void OnValidate()
     {
