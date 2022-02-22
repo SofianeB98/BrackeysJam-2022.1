44d0c74b70068aa51357284d684c6fc618fddd6f
Sofiane <sofiane.benembareck@hotmail.com>
2022-02-22 23:37:36 +0100

    Add shoot ability + update scene

--- Assets/Scripts/Projectiles/ProjectilesData/ProjectileData.cs
+++ Assets/Scripts/Projectiles/ProjectilesData/ProjectileData.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterProjectileData : ScriptableObject
+[CreateAssetMenu(fileName = "ProjectileData", menuName = "Projectile/New Projectile Data", order = 0)]
+public class ProjectileData : ScriptableObject
 {
     public float Damage = 25.0f;
     public float LifeTime = 1.0f;
     public float Speed = 15.0f;
     public float DetectionRadius = 2.0f;
+    public float ProjectileHeight = 2.0f;
 }
