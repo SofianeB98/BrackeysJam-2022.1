467792355604474b8cec06f506af9b592ca9420d
Sofiane <sofiane.benembareck@hotmail.com>
2022-02-23 10:19:56 +0100

    Can update projectile effect with data

--- Assets/Scripts/Projectiles/CharacterProjectile.cs
+++ Assets/Scripts/Projectiles/CharacterProjectile.cs
@@ -8,12 +8,17 @@ public class CharacterProjectile : MonoBehaviour
     [Header("Projectile Data")] 
     [SerializeField] private ProjectileData m_ProjectileData;
     [SerializeField] private LayerMask m_IgnoreLayer;
-    
+    [SerializeField] private ParticleSystem m_Effect;
+
+    private float m_LifeTime = 1.0f;
     private Vector3 m_DetectionPoint;
     
     private void Start()
     {
-        Destroy(gameObject, m_ProjectileData.LifeTime);
+        m_LifeTime = (m_ProjectileData.DistanceToReach / m_ProjectileData.Speed);
+        InitializeParticleSystem();
+        
+        Destroy(gameObject, m_LifeTime + 0.02f);
         m_DetectionPoint = transform.position;
     }
 
@@ -23,6 +28,16 @@ public class CharacterProjectile : MonoBehaviour
         Move();
     }
 
+    private void InitializeParticleSystem()
+    {
+        var mainModule = m_Effect.main;
+        mainModule.duration = m_LifeTime + 0.02f;
+        mainModule.startSpeed = m_ProjectileData.Speed;
+        mainModule.startLifetime = m_LifeTime;
+        
+        m_Effect.gameObject.SetActive(true);
+    }
+    
     private void Detect()
     {
         Collider[] cols = Physics.OverlapCapsule(m_DetectionPoint, m_DetectionPoint + Vector3.up * m_ProjectileData.ProjectileHeight, m_ProjectileData.DetectionRadius, ~m_IgnoreLayer);
--- Assets/Scripts/Projectiles/ProjectilesData/ProjectileData.cs
+++ Assets/Scripts/Projectiles/ProjectilesData/ProjectileData.cs
@@ -5,9 +5,19 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "ProjectileData", menuName = "Projectile/New Projectile Data", order = 0)]
 public class ProjectileData : ScriptableObject
 {
+    [Header("Projectile")]
     public float Damage = 25.0f;
-    public float LifeTime = 1.0f;
+    public float DistanceToReach = 20.0f;
     public float Speed = 15.0f;
     public float DetectionRadius = 2.0f;
     public float ProjectileHeight = 2.0f;
+    
+    [Header("DONT TOUCH THESE FIELDS !")]
+    [Tooltip("in seconds")] public float LifeTime = 1.0f;
+    
+    private void OnValidate()
+    {
+        if (Speed > 0.0f && DistanceToReach > 0.0f)
+            LifeTime = DistanceToReach / Speed;
+    }
 }
