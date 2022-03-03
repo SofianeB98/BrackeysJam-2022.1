d9f3432f2be19746c5cc32044c8dec586ea4403e
Sofiane <sofiane.benembareck@hotmail.com>
2022-03-03 15:27:19 +0100

    Fix projectile collision
    the collision start an additionnal size to the initial detection size. The additionnal is calculated from the previous position.

--- Assets/Scripts/Projectiles/Projectile.cs
+++ Assets/Scripts/Projectiles/Projectile.cs
@@ -7,30 +7,33 @@ using UnityEngine.VFX;
 public class Projectile : MonoBehaviour
 {
     public event Action<Transform, float> CollisionDetectedEvent;
-    
-    [Header("Projectile Data")] 
-    [SerializeField] private ProjectileData m_ProjectileData;
+
+    [Header("Projectile Data")] [SerializeField]
+    private ProjectileData m_ProjectileData;
+
     [SerializeField] private LayerMask m_IgnoreLayer;
     [SerializeField] private bool m_IsVfxGraph = false;
     [SerializeField] private ParticleSystem m_Effect;
     [SerializeField] private ParticleSystem m_GlitchedEffect;
     [SerializeField] private bool m_IsNotReal = false;
     [SerializeField] private VisualEffect ShaderGraphFX;
-    
-    [Header("Gizmo")] 
-    [SerializeField] private Mesh m_CubeMesh;
-    
+
+    [Header("Gizmo")] [SerializeField] private Mesh m_CubeMesh;
+
     private Vector3 m_DetectionPoint;
+    private Vector3 m_PreviousDetectionPoint;
+    private bool m_PreviousPointInitialized = false;
 
     private bool m_CanDestroy = false;
-    
+
     private void Start()
     {
         if (!m_IsVfxGraph)
             InitializeParticleSystem();
-        
+
         Destroy(gameObject, m_ProjectileData.LifeTime + 1.0f);
         m_DetectionPoint = transform.position;
+        m_PreviousPointInitialized = true;
     }
 
     public void SetNotReal(bool v)
@@ -41,16 +44,17 @@ public class Projectile : MonoBehaviour
             ShaderGraphFX.SetBool("Glitch", m_IsNotReal);
         }
     }
-    
+
     private void Update()
     {
         if (m_CanDestroy)
             return;
         
+        m_PreviousDetectionPoint = m_DetectionPoint;
+        Move();
+
         if (!m_IsNotReal)
             Detect();
-        
-        Move();
     }
 
     private void InitializeParticleSystem()
@@ -68,14 +72,21 @@ public class Projectile : MonoBehaviour
         else
             m_Effect.gameObject.SetActive(true);
     }
-    
+
     private void Detect()
     {
         if (m_CanDestroy)
             return;
-        
-        var cols = Physics.OverlapBox(m_DetectionPoint + (transform.rotation * m_ProjectileData.DetectionPointOffset), m_ProjectileData.DetectionSize * 0.5f, transform.rotation, ~m_IgnoreLayer);
-        
+
+        float dist = Vector3.Distance(m_DetectionPoint, m_PreviousDetectionPoint);
+        float addingPercent = dist / m_ProjectileData.DetectionSize.z;
+        float adding = m_ProjectileData.DetectionSize.z * addingPercent;
+        var addingOffset = Vector3.forward * adding;
+
+        var cols = Physics.OverlapBox(
+            m_DetectionPoint + (transform.rotation * (m_ProjectileData.DetectionPointOffset - addingOffset)),
+            (m_ProjectileData.DetectionSize + addingOffset) * 0.5f, transform.rotation, ~m_IgnoreLayer);
+
         if (cols == null || cols.Length == 0)
         {
             Debug.Log("No collision detected");
@@ -87,7 +98,8 @@ public class Projectile : MonoBehaviour
             Debug.Log("COLLIDE : " + cols[i].gameObject.name);
             CollisionDetectedEvent?.Invoke(cols[i].transform, m_ProjectileData.Damage);
         }
-        
+
+
         DestroyProjectile();
     }
 
@@ -97,7 +109,7 @@ public class Projectile : MonoBehaviour
         if (m_IsVfxGraph)
             transform.position = m_DetectionPoint;
     }
-    
+
     private void DestroyProjectile()
     {
         m_CanDestroy = true;
@@ -106,13 +118,33 @@ public class Projectile : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-        if (m_IsNotReal)
+        if (m_IsNotReal || m_CanDestroy)
             return;
-        
+
         Gizmos.color = Color.cyan;
-        
+
         if (m_CubeMesh != null)
-            Gizmos.DrawWireMesh(m_CubeMesh, m_DetectionPoint + (transform.rotation * m_ProjectileData.DetectionPointOffset), transform.rotation, m_ProjectileData.DetectionSize);
-        
+        {
+            float dist = m_PreviousPointInitialized ? Vector3.Distance(m_DetectionPoint, m_PreviousDetectionPoint) : 0f;
+            float addingPercent = dist / m_ProjectileData.DetectionSize.z;
+            float adding = m_ProjectileData.DetectionSize.z * addingPercent;
+
+            Gizmos.DrawWireMesh(m_CubeMesh,
+                m_DetectionPoint + (transform.rotation * m_ProjectileData.DetectionPointOffset), transform.rotation,
+                m_ProjectileData.DetectionSize);
+            Gizmos.color = Color.blue;
+            var size = m_ProjectileData.DetectionSize;
+            size.x *= 0.5f;
+            size.y *= 0.5f;
+            var addingOffset = Vector3.forward * adding;
+            Gizmos.DrawWireMesh(m_CubeMesh,
+                m_DetectionPoint + (transform.rotation * (m_ProjectileData.DetectionPointOffset - addingOffset * 0.5f)),
+                transform.rotation, size + addingOffset);
+        }
+
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawSphere(m_DetectionPoint + Vector3.up * 0.1f, 0.1f);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawSphere(m_PreviousDetectionPoint + Vector3.up * 0.1f, 0.1f);
     }
 }
